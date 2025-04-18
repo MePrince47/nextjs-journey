3fa7e174f2a92d89597cbcafc2866645f2c4d838
MePrince47 <ewondjojoseph@gmail.com>
2025-04-18 17:13:15 +0100

    projet du jour 2 V2 no Bugg

--- jour-02/compteur/src/components/Compteur.jsx
+++ jour-02/compteur/src/components/Compteur.jsx
@@ -3,14 +3,12 @@ import { useState } from 'react';
 
 export default function Compteur(){
     const [count, setCount] = useState(0);
-    return(
-        <div style={{ textAlign: 'center',
-            marginTop: '50px'
-        }}>
-        <h1>Compteur : {{count}} </h1>
+return(
+    <div style={{ textAlign: 'center',marginTop: '50px'}}>
+        <h1>Compteur : {count}</h1>
         <button onClick={()=> setCount(count + 1 )}> ➕ Incrémenter </button>
         <button onClick={()=> setCount(count - 1 )}> ➖ Décrémenter </button>
         <button onClick={()=> setCount(0)}>🔄 Reset</button>
         </div>
-    )
+    );
 }
