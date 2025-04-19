9a1d41464a8b5eaa3b8ce7f75aecbb85a8b2cdac
MePrince47 <ewondjojoseph@gmail.com>
2025-04-19 20:51:04 +0100

    second commint for end code V1

---
+++ jour-03/Afficheur_utilisateur_fetch/src/components/UserList.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState} from 'react';
+
+export default function UserList(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users') //Mon API
+    .then(res => {
+        if(!res.ok) throw new Error('Erreur de chargement');
+        return res.json();
+    })
+    .then(data => {
+        setUsers(data);
+        setLoading(false);
+    })
+    .catch(err => {
+        setError(err.message);
+        setLoading(false);
+    });
+}, []);
+
+if (loading) return <p>Chargement ...</p>;
+if (error) return <p>Erreur: {error} </p>;
+
+return (
+    <div style={{textAlign: 'center'}} >
+        <h2>Liste des utilisateurs</h2>
+        <ul>
+            {users.map(user => (
+                <li key={user.id}> {user.name} - {user.email} </li>
+            ))}
+        </ul>
+
+    </div>
+);
+
+
+}
